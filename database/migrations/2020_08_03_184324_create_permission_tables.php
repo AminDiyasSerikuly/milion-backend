84ad915543305ad0844b4f968321d37d28b610d0
AminDiyasSerikuly <amindiyass@gmail.com>
2020-08-21 00:46:58 +0600

    complete bug:fix migration error

--- database/migrations/2020_08_03_184324_create_permission_tables.php
+++ database/migrations/2020_08_03_184324_create_permission_tables.php
@@ -11,6 +11,7 @@ class CreatePermissionTables extends Migration
      *
      * @return void
      */
+    //
     public function up()
     {
         $tableNames = config('permission.table_names');
