143a3e08023c412fbc6856514d89ec29202e8f70
AminDiyasSerikuly <amindiyass@gmail.com>
2021-02-07 22:07:43 +0600

    check

--- resources/views/schedule/show.blade.php
+++ resources/views/schedule/show.blade.php
@@ -42,122 +42,122 @@ $groups = $groups->pluck('name', 'id')->toArray();
                     </tr>
                     </thead>
                     <tbody>
-                    @foreach(Schedule::getLessonTimes($day->week_day_number) as $time)
-                        <tr>
-                            <td>{{$time}}</td>
-                            @foreach($table_cabinets as $cabinet)
-                                <?php
-                                isset($schedules_array[$day->week_day_number][$time][$cabinet->id]) ? ($schedule = $schedules_array[$day->week_day_number][$time][$cabinet->id]) : $schedule = null
-                                ?>
-                                <td>
-                                    @if(isset($schedule))
-                                        <div class="card"
-                                             style="background-color: #f6fdff; border: 1px solid lightgrey;">
-                                            <div class="card-header"><i class="fa fa-users"></i> {{$schedule['group']}}
-                                            </div>
-                                            <div class="card-body">
+{{--                    @foreach(Schedule::getLessonTimes($day->week_day_number) as $time)--}}
+{{--                        <tr>--}}
+{{--                            <td>{{$time}}</td>--}}
+{{--                            @foreach($table_cabinets as $cabinet)--}}
+{{--                                <?php--}}
+{{--                                isset($schedules_array[$day->week_day_number][$time][$cabinet->id]) ? ($schedule = $schedules_array[$day->week_day_number][$time][$cabinet->id]) : $schedule = null--}}
+{{--                                ?>--}}
+{{--                                <td>--}}
+{{--                                    @if(isset($schedule))--}}
+{{--                                        <div class="card"--}}
+{{--                                             style="background-color: #f6fdff; border: 1px solid lightgrey;">--}}
+{{--                                            <div class="card-header"><i class="fa fa-users"></i> {{$schedule['group']}}--}}
+{{--                                            </div>--}}
+{{--                                            <div class="card-body">--}}
 
-                                                <i class="fa fa-book"></i> Предмет: {{$schedule['subject_title']}}
-                                                <br>
-                                                <i class="fa fa-user"></i> Учитель: Д.Амин
-                                                {!! Form::open(['method' => 'POST', 'route' => 'schedule.delete','onsubmit' => 'return confirmDelete()']); !!}
-                                                @csrf
-                                                <input type="hidden" name="schedule_id"
-                                                       value="{{$schedule['schedule_id']}}">
-                                                <button class="float-right"
-                                                        style="padding-right:0; border: none; background-color: white;"
-                                                        type="submit">
-                                                    <i style="font-size:90%;color: red"
-                                                       class="float-right fa fa-trash"></i>
-                                                </button>
-                                                {!! Form::close(); !!}
-                                                <button value="{{$schedule['schedule_id']}}"
-                                                        class="float-right get_lesson_data"
-                                                        style="padding-right:0;border: none; background-color: white;">
-                                                    <i style="font-size:90%;color: blue"
-                                                       class="float-right fa fa-edit"></i>
-                                                </button>
-                                            </div>
-                                        </div>
-                                    @else
-                                        {{'Не указано'}}
-                                    @endif
-                                </td>
-                            @endforeach
-                        </tr>
-                    @endforeach
+{{--                                                <i class="fa fa-book"></i> Предмет: {{$schedule['subject_title']}}--}}
+{{--                                                <br>--}}
+{{--                                                <i class="fa fa-user"></i> Учитель: Д.Амин--}}
+{{--                                                {!! Form::open(['method' => 'POST', 'route' => 'schedule.delete','onsubmit' => 'return confirmDelete()']); !!}--}}
+{{--                                                @csrf--}}
+{{--                                                <input type="hidden" name="schedule_id"--}}
+{{--                                                       value="{{$schedule['schedule_id']}}">--}}
+{{--                                                <button class="float-right"--}}
+{{--                                                        style="padding-right:0; border: none; background-color: white;"--}}
+{{--                                                        type="submit">--}}
+{{--                                                    <i style="font-size:90%;color: red"--}}
+{{--                                                       class="float-right fa fa-trash"></i>--}}
+{{--                                                </button>--}}
+{{--                                                {!! Form::close(); !!}--}}
+{{--                                                <button value="{{$schedule['schedule_id']}}"--}}
+{{--                                                        class="float-right get_lesson_data"--}}
+{{--                                                        style="padding-right:0;border: none; background-color: white;">--}}
+{{--                                                    <i style="font-size:90%;color: blue"--}}
+{{--                                                       class="float-right fa fa-edit"></i>--}}
+{{--                                                </button>--}}
+{{--                                            </div>--}}
+{{--                                        </div>--}}
+{{--                                    @else--}}
+{{--                                        {{'Не указано'}}--}}
+{{--                                    @endif--}}
+{{--                                </td>--}}
+{{--                            @endforeach--}}
+{{--                        </tr>--}}
+{{--                    @endforeach--}}
                     </tbody>
                 </table>
             </div>
         @endforeach
     </div>
 
-{{--    <div class="modal fade" id="add_lesson_modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"--}}
-{{--         aria-hidden="true">--}}
-{{--        <div class="modal-dialog" role="document">--}}
-{{--            <div class="modal-content">--}}
-{{--                <div class="modal-header">--}}
-{{--                    <h5 class="modal-title" id="exampleModalLabel">--}}
-{{--                        Расписание уроков</h5>--}}
-{{--                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">--}}
-{{--                        <span aria-hidden="true">&times;</span>--}}
-{{--                    </button>--}}
-{{--                </div>--}}
-{{--                {{Form::open(['route' => 'schedule.add', 'method' => 'POST', 'id' => 'schedule_add_form'])}}--}}
-{{--                {{Form::token()}}--}}
-{{--                <div class="modal-body">--}}
-{{--                    <div class="form-group">--}}
-{{--                        <label>Выберите день недели</label>--}}
-{{--                        {!! Form::select('week_day_id', [null => 'Выберите день'] +--}}
-{{--                           $weekdays,--}}
-{{--                            null,--}}
-{{--                            [--}}
-{{--                                'class' => 'form-control',--}}
-{{--                                'id' => 'week_day_id',--}}
-{{--                                ])--}}
-{{--                         !!}--}}
-{{--                    </div>--}}
-{{--                    <div class="form-group">--}}
-{{--                        <label>Укажите номер кабинета</label>--}}
-{{--                        {!! Form::select('cabinet_id', [null => 'Укажите номер кабинета'] +--}}
-{{--                           $cabinets,--}}
-{{--                            null,--}}
-{{--                            [--}}
-{{--                                'class' => 'form-control',--}}
-{{--                                'id' => 'cabinet_id',--}}
-{{--                            ])--}}
-{{--                         !!}--}}
-{{--                    </div>--}}
-{{--                    <div class="form-group d-none" id="lesson_begin_select">--}}
-{{--                        <label>Укажите время</label>--}}
-{{--                        {!! Form::select('lesson_time', [null => 'Укажите время'] +--}}
-{{--                             [],--}}
-{{--                              null,--}}
-{{--                              ['class' => 'form-control', 'id' => 'lesson_time'])--}}
-{{--                           !!}--}}
-{{--                    </div>--}}
-{{--                    <div class="form-group">--}}
-{{--                        <label>Выбирает группу</label>--}}
-{{--                        {!! Form::select('group_id', [null => 'Укажите группу'] +--}}
-{{--                           $groups,--}}
-{{--                            null,--}}
-{{--                            [--}}
-{{--                                'class' => 'form-control',--}}
-{{--                                'id' => 'group_id'--}}
-{{--                            ])--}}
-{{--                         !!}--}}
-{{--                        <input type="hidden" id="schedule_id" name="schedule_id">--}}
-{{--                    </div>--}}
+    <div class="modal fade" id="add_lesson_modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+         aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">
+                        Расписание уроков</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                {{Form::open(['route' => 'schedule.add', 'method' => 'POST', 'id' => 'schedule_add_form'])}}
+                {{Form::token()}}
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label>Выберите день недели</label>
+                        {!! Form::select('week_day_id', [null => 'Выберите день'] +
+                           $weekdays,
+                            null,
+                            [
+                                'class' => 'form-control',
+                                'id' => 'week_day_id',
+                                ])
+                         !!}
+                    </div>
+                    <div class="form-group">
+                        <label>Укажите номер кабинета</label>
+                        {!! Form::select('cabinet_id', [null => 'Укажите номер кабинета'] +
+                           $cabinets,
+                            null,
+                            [
+                                'class' => 'form-control',
+                                'id' => 'cabinet_id',
+                            ])
+                         !!}
+                    </div>
+                    <div class="form-group d-none" id="lesson_begin_select">
+                        <label>Укажите время</label>
+                        {!! Form::select('lesson_time', [null => 'Укажите время'] +
+                             [],
+                              null,
+                              ['class' => 'form-control', 'id' => 'lesson_time'])
+                           !!}
+                    </div>
+                    <div class="form-group">
+                        <label>Выбирает группу</label>
+                        {!! Form::select('group_id', [null => 'Укажите группу'] +
+                           $groups,
+                            null,
+                            [
+                                'class' => 'form-control',
+                                'id' => 'group_id'
+                            ])
+                         !!}
+                        <input type="hidden" id="schedule_id" name="schedule_id">
+                    </div>
 
-{{--                </div>--}}
-{{--                <div class="modal-footer">--}}
-{{--                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Закрыть</button>--}}
-{{--                    <button id="lesson_add_submit_button" type="submit" class="btn btn-primary">Добавить</button>--}}
-{{--                </div>--}}
-{{--                {{Form::close()}}--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--    </div>--}}
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Закрыть</button>
+                    <button id="lesson_add_submit_button" type="submit" class="btn btn-primary">Добавить</button>
+                </div>
+                {{Form::close()}}
+            </div>
+        </div>
+    </div>
 @endsection
 @section('js')
     <script>
