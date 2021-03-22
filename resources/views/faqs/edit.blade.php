7d3a635749f55dbab3d53c996bb86ef083343709
AminDiyasSerikuly <amindiyass@gmail.com>
2021-03-23 02:43:33 +0600

    fix bug in faqs

--- resources/views/faqs/edit.blade.php
+++ resources/views/faqs/edit.blade.php
@@ -10,43 +10,43 @@
                     <h3 class="card-title">Форма для заполнение FAQ</h3>
                 </div>
 
-                {{Form::open(['route' => 'faq.store', 'method' => 'POST'])}}
-                {{Form::token()}}
-                <div class="card-body">
-                    <div class="form-group">
-                        <label>Вопрос</label>
-                        <div class="input-group">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text"><i class="fas fa-question-circle"></i></span>
+                <form action="{{route('faq.update', ['faq' => $faq])}}" method="POST" >
+                    @method('PUT')
+                    @csrf
+                    <div class="card-body">
+                        <div class="form-group">
+                            <label>Вопрос</label>
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text"><i class="fas fa-question-circle"></i></span>
+                                </div>
+
+                                {!! Form::textarea('question',$faq->question,[
+                                            'class' => 'form-control',
+                                            'rows' => 5,
+                                            ]) !!}
                             </div>
-
-                            {!! Form::textarea('question',$faq->question,[
-                                        'class' => 'form-control',
-                                        'rows' => 5,
-                                        ]) !!}
                         </div>
-                    </div>
-                    <div class="form-group">
-                        <label>Ответ</label>
-                        <div class="input-group">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text"><i class="fas fa-check-circle"></i></span>
+                        <div class="form-group">
+                            <label>Ответ</label>
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text"><i class="fas fa-check-circle"></i></span>
+                                </div>
+
+                                {!! Form::textarea('answer',$faq->answer,[
+                                            'class' => 'form-control',
+                                            'rows' => 5
+                                            ]) !!}
                             </div>
-
-                            {!! Form::textarea('answer',$faq->answer,[
-                                        'class' => 'form-control',
-                                        'rows' => 5
-                                        ]) !!}
                         </div>
+
+
+                        <button type="submit" class="btn btn-primary">
+                            Сохранить
+                        </button>
                     </div>
-
-
-                    <button type="submit" class="btn btn-primary">
-                        Сохранить
-                    </button>
-                </div>
-
-                {{Form::close()}}
+                </form>
             </div>
         </div>
     </div>
